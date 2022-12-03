f389331bd2b6873393458f826ee7e31aff307ac1
stonekite <feelsknight@gmail.com>
2022-12-03 03:01:06 +0300

    restructured day 1 solutions

--- src/day1-1.cljs
+++ src/day1-1.cljs
@@ -2,10 +2,11 @@
 
 (def fs (js/require "fs"))
 
-(defn sumblock [block]
-  (reduce (fn [a b] (+ a (#(js/parseInt %) b))) 0 (str/split block #"\n")))
+(def blocks (str/split (.readFileSync fs "./inputs/day1.txt" "utf8") "\n\n"))
 
-(let [blocks (str/split (.readFileSync fs "./inputs/day1.txt" "utf8") "\n\n")] 
-  (reduce (fn [a b]  
-            (if (> (sumblock a) (sumblock b)) 
-              (sumblock a) (sumblock b))) blocks))
+(def sumblock (memoize (fn [block]
+                         (reduce (fn [a b] (+ a (#(js/parseInt %) b))) 0 (str/split block #"\n")))))
+
+(reduce (fn [a block]
+          (if (> (sumblock a) (sumblock block))
+            (sumblock a) (sumblock block))) blocks)
