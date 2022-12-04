d8390e85ede58bfa4e4b56aeaaff71d5c3009624
stonekite <feelsknight@gmail.com>
2022-12-04 15:25:58 +0300

    solved day 4

---
+++ src/day4-1.cljs
@@ -0,0 +1,20 @@
+(require '[clojure.string :as str])
+
+(def fs (js/require "fs"))
+
+(def lines (str/split (.readFileSync fs "./inputs/day4.txt" "utf8") "\n"))
+
+(defn solve-line [line]
+  (let [[left right] (sort-by count (map (fn [group] 
+                                           (let [[lower upper] (str/split group "-")]  
+                                             (range  
+                                              (#(js/parseInt %) lower)  
+                                              (+ 1 (#(js/parseInt %) upper)))))  
+                                         (str/split line ",")))]
+    (every? 
+     (fn [l-id] (some
+                 (fn [r-id] (= l-id r-id))
+                 right))
+     left)))
+
+(count (filter solve-line lines))
